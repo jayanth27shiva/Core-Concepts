08cd8a97bace5d70a6f03ae8f2a61bb9f0d33f74
jayanth27shiva <83591166+jayanth27shiva@users.noreply.github.com>
2022-10-28 10:57:23 +0530

    Create Doubly LL insertion.java

---
+++ Data Structures/LinkedList/Doubly LL insertion.java
@@ -0,0 +1,65 @@
+class Node {
+    int data;
+    Node prev,next;
+} 
+class linkedList {
+    Node head = null;
+    Node createNode(int data) {
+        Node nn = new Node();
+        nn.data=data;
+        nn.next=null;
+        nn.prev=null;
+        return nn;
+    } 
+    void insertAtBeginning(int data) {
+        Node nn = createNode(data);
+        if(head==null) {
+            head=nn;
+        } else {
+            nn.next=head;
+            head.prev=nn;
+            head=nn;
+        }
+    }
+    void insertAtEnd(int data) {
+        Node nn = createNode(data);
+        Node temp=head;
+        while(temp.next!=null) {
+            temp=temp.next;
+        } temp.next=nn;
+        nn.prev=temp;
+    }
+    void insertAtPosition(int data,int loc) {
+        Node nn = createNode(data);
+        Node temp=head;
+        Node pre=null;
+        while(--loc>0) {
+            pre=temp;
+            temp=temp.next;
+        } nn.next=temp;
+        nn.prev=pre;
+        pre.next=nn;
+        temp.prev=nn;
+    }
+    void display() {
+        Node temp=head;
+        while(temp!=null) {
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+}
+class Main{
+    public static void main(String[] args) {
+    linkedList L = new linkedList();
+    L.insertAtBeginning(10);
+    L.insertAtBeginning(20);
+    L.display();
+    L.insertAtEnd(30);
+    L.insertAtEnd(40);
+    L.display();
+    L.insertAtPosition(25,2);
+    L.display();
+    }
+}
