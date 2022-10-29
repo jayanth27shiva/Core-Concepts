cec22f79df8bfcf42dc5ffcc31e0e9863d0ddef9
jayanth27shiva <83591166+jayanth27shiva@users.noreply.github.com>
2022-10-29 19:18:40 +0530

    Deletion in SinglyLL

--- Data Structures/LinkedList/Singly LL Deletion.java
+++ Data Structures/LinkedList/Singly LL Deletion.java
@@ -11,31 +11,13 @@ class linkedList {
         nn.data=data;
         nn.next=null;
         return nn;
-    } 
-    void insertAtbeginning(int data) {
-        Node nn=createNode(data);
-        if(head==null) {
-            head=nn;
-        } else {
-        nn.next=head;
-        head=nn;
-        }
     }
-    void insertAtEnd(int data) {
+    void insertion(int data) {
         Node nn=createNode(data);
         Node temp = head;
         while(temp.next!=null) {
             temp=temp.next;
         } 
-        temp.next=nn;
-    }
-    void insertAtSpecificPos(int data,int loc) {
-        Node nn=createNode(data);
-        Node temp = head;
-        for(int index=0;index<loc-1;index++) {
-            temp=temp.next;
-        } 
-        nn.next=temp.next;
         temp.next=nn;
     }
     void deleteAtBeginning() {
@@ -61,12 +43,14 @@ class linkedList {
         prev.next=temp.next;
     }
     void display() {
-        Node temp=head;
+        Node temp = head;
         while(temp!=null) {
-            System.out.print(temp.data+" ");
-            temp=temp.next;
+            System.out.print(temp.data);
+            if(temp.next!=null) {
+                System.out.print("->");
+            }
+            temp = temp.next;
         }
-        System.out.print("null");
         System.out.println();
     }
 }
@@ -74,15 +58,14 @@ class linkedList {
 public class Main
 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		linkedList L = new linkedList();
-		L.insertAtbeginning(10);
-		L.insertAtbeginning(20);
-		L.display();
-		L.insertAtEnd(5);
-		L.insertAtEnd(50);
-		L.display();
-		L.insertAtSpecificPos(30,3);
-		L.display();
+		int n = sc.nextInt();
+	    	for(int index=0;index<n;index++) {
+	        	int data = sc.nextInt();
+	        	L.insertion(data);
+	    	}
+	    	L.display();
 		L.deleteAtBeginning();
 		L.display();
 		L.deleteAtEnd();
