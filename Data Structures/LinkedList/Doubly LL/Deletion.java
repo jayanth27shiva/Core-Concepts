c73e1f37345441b3d2bb051081f3787ab3f8cbf0
jayanth27shiva <83591166+jayanth27shiva@users.noreply.github.com>
2022-10-29 19:26:27 +0530

    Deletion in DoublyLL

--- Data Structures/LinkedList/Doubly LL/Deletion.java
+++ Data Structures/LinkedList/Doubly LL/Deletion.java
@@ -11,17 +11,7 @@ class linkedList {
         nn.prev=null;
         return nn;
     } 
-    void insertAtBeginning(int data) {
-        Node nn = createNode(data);
-        if(head==null) {
-            head=nn;
-        } else {
-            nn.next=head;
-            head.prev=nn;
-            head=nn;
-        }
-    }
-    void insertAtEnd(int data) {
+    void insertion(int data) {
         Node nn = createNode(data);
         Node temp=head;
         while(temp.next!=null) {
@@ -29,23 +19,14 @@ class linkedList {
         } temp.next=nn;
         nn.prev=temp;
     }
-    void insertAtPosition(int data,int loc) {
-        Node nn = createNode(data);
-        Node temp=head;
-        Node pre=null;
-        while(--loc>0) {
-            pre=temp;
-            temp=temp.next;
-        } nn.next=temp;
-        nn.prev=pre;
-        pre.next=nn;
-        temp.prev=nn;
-    }
     void display() {
-        Node temp=head;
+        Node temp = head;
         while(temp!=null) {
-            System.out.print(temp.data+" ");
-            temp=temp.next;
+            System.out.print(temp.data);
+            if(temp.next!=null) {
+                System.out.print("->");
+            }
+            temp = temp.next;
         }
         System.out.println();
     }
@@ -72,13 +53,12 @@ class linkedList {
 class Main{
     public static void main(String[] args) {
     linkedList L = new linkedList();
-    L.insertAtBeginning(10);
-    L.insertAtBeginning(20);
-    L.display();
-    L.insertAtEnd(30);
-    L.insertAtEnd(40);
-    L.display();
-    L.insertAtPosition(25,2);
+    int n = sc.nextInt();
+	for(int index=0;index<n;index++)
+	{
+	    int data = sc.nextInt();
+	    L.insertion(data);
+	}
     L.display();
     L.deleteAtBegining();
     L.display();
