4d144139aee194f59b1feda974467212fcb24240
jayanth27shiva <83591166+jayanth27shiva@users.noreply.github.com>
2022-10-29 19:27:29 +0530

    Insertion in DoublyLL

--- Data Structures/LinkedList/Doubly LL/Insertion.java
+++ Data Structures/LinkedList/Doubly LL/Insertion.java
@@ -42,10 +42,13 @@ class linkedList {
         temp.prev=nn;
     }
     void display() {
-        Node temp=head;
+        Node temp = head;
         while(temp!=null) {
-            System.out.print(temp.data+" ");
-            temp=temp.next;
+            System.out.print(temp.data);
+            if(temp.next!=null) {
+                System.out.print("->");
+            }
+            temp = temp.next;
         }
         System.out.println();
     }
